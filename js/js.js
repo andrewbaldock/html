4ac427f4d7c0a9b37e7d6dbeaff243046b7ff821
Andrew Baldock <andrewbaldock@yahoo.com>
2013-07-30 22:50:11 -0700

    nav states fixed

--- js/js.js
+++ js/js.js
@@ -9,28 +9,68 @@
 d.extend(true,d,{support:{rgba:m()}});var k=["color","backgroundColor","borderBottomColor","borderLeftColor","borderRightColor","borderTopColor","outlineColor"];d.each(k,function(b,a){d.Tween.propHooks[a]={get:function(c){return d(c.elem).css(a)},set:function(c){var e=c.elem.style,i=g(d(c.elem).css(a)),h=g(c.end);c.run=function(f){e[a]=j(i,h,f)}}}});d.Tween.propHooks.borderColor={set:function(b){var a=b.elem.style,c=[],e=k.slice(2,6);d.each(e,function(h,f){c[f]=g(d(b.elem).css(f))});var i=g(b.end);
 b.run=function(h){d.each(e,function(f,l){a[l]=j(c[l],i,h)})}}}})(jQuery);
 
-//get slide height
+//namespacing
+var andrew = {};
+andrew.tinyMenuShowing = false;
 
-function sliderHeight() {
+//report to console
+andrew.logThis = function(wut){
+	if(typeof console != 'undefined'){
+		console.log(wut);
+	}
+}
+
+//report nav state
+var navDisplayState = '';
+andrew.navDisplayStateUpdate = function (){
+	navDisplayState = $('.navigation').css('display');
+	andrew.logthis(navDisplayState);
+}
+
+//get slide height
+andrew.sliderHeight = function() {
     wh = $(window).height();
     $('#slide1').css({
         height: wh
     });
 }
 
-//centers homepage blurb
+//what size 'device'
+andrew.wdevice = 'desktop';
+andrew.wwUpdate = function(){
+	andrew.ww = $(window).width();
+	if(andrew.ww >= 1024){andrew.wdevice = 'desktop'};
+	if(andrew.ww >= 767 && andrew.ww < 1024){andrew.wdevice = 'laptop'};
+	if(andrew.ww > 480 && andrew.ww < 767){andrew.wdevice = 'tablet'};
+	if(andrew.ww <= 480){andrew.wdevice = 'phone'};
+	andrew.logThis(andrew.wdevice);
+}
+andrew.navStay = function() {
+	andrew.wwUpdate();
+	if(andrew.wdevice == 'desktop')	{$('.navigation').css('display','inline-block!important');}
+	if(andrew.wdevice == 'laptop')	{$('.navigation').css('display','inline-block!important');};
+	if(andrew.wdevice == 'phone' || andrew.wdevice == 'tablet' ){ 
+		if(andrew.tinyMenuShowing == false)	{
+			$('.navigation').css('display','none');
+		} else {
+			$('.navigation').css('display','block');
+		}
+	}
+}
 
-function mymargtop() {
+//centers homepage blurb
+andrew.mymargtop = function() {
     var body_h = $(window).height();
     var container_h = $('.filtr_bg').height();
     var marg_top = Math.abs((body_h - container_h) / 3);
     $('.filtr_bg').css('margin-top', marg_top);
     $('.filtr_bg').css('margin-bottom', marg_top);
+    
+    andrew.colorMeBadd();
 }
 
 //color animation
-
-function colorMeBadd() {
+andrew.colorMeBadd = function() {
     $(".colormebadd").animate({
         color: '#CF2CC7'
     }, 3000, function () {
@@ -55,24 +95,20 @@ jQuery(document).ready(function ($) {
     htmlbody = $('html,body');
 
     /**/
-    if (mywindow.scrollTop() < 10) {
+    if (mywindow.scrollTop() < 25) {
         $('.navigation li[data-slide="1"]').addClass('active');
     }
     /**/
 
     slide.waypoint(function (event, direction) {
-
         dataslide = $(this).attr('data-slide');
-
+				$('.navigation li').removeClass('active');
+				$('.navigation li[data-slide="' + dataslide + '"]').addClass('active')
         if (direction === 'down') {
-            $('.navigation li[data-slide="' + dataslide + '"]').addClass('active').prev().removeClass('active');
-
-            $('.navigation li[data-slide="1"]').removeClass('active');
-
+           // do sometin
         } else {
-            $('.navigation li[data-slide="' + dataslide + '"]').addClass('active').next().removeClass('active');
+					// do sometin else
         }
-
     });
 
     mywindow.scroll(function () {
@@ -119,13 +155,27 @@ jQuery(document).ready(function ($) {
         topSpacing: 0
     });
 
+		//NAVIGATION MENU - top sticky bar and phone-size floating menu toggle
+		andrew.wwUpdate(); //screen size type
+		
 		$(".btn_dropdown").click(function () {
-				$(".navigation").slideToggle("slow");
+				if(andrew.tinyMenuShowing == false) {
+					 $(".navigation").show("slow");
+					 andrew.tinyMenuShowing = true;
+				} else {
+					 $(".navigation").hide("slow");
+					 andrew.tinyMenuShowing = false;
+				}
+				andrew.navStay();
 		});
+		
 		$(".navigation li").click(function () {
-				$(".navigation").hide("fast");
+				if(andrew.wdevice == 'phone' || andrew.wdevice == 'tablet') {
+						$(".navigation").hide("fast");
+						andrew.tinyMenuShowing = false;
+						andrew.navStay();
+				}
 		});
-
 
 
     //prettyPhoto
@@ -150,37 +200,20 @@ jQuery(document).ready(function ($) {
             'background': 'none'
         });
     });
-
+    
     $("#slide1").each(function () {
         var slide_h = $(this).height();
-
         $(this).css('background-size', '100% ' + slide_h + 'px');
-
     });
-
-
-
-    sliderHeight();
-
-    mymargtop();
-
-    colorMeBadd();
-
+    andrew.wwUpdate();
+    andrew.sliderHeight();
+    andrew.mymargtop();
 });
 
 $(window).bind('resize', function () {
-
-    //Update slider height
-    sliderHeight();
-
-    mymargtop();
-
-    colorMeBadd();
-    
-     //DROP menu	
-    if ($(window).width() > 750) {
-			$(".navigation").css('display','inline-block');
-    }
-
-
+		andrew.wwUpdate();
+    andrew.sliderHeight();
+    andrew.mymargtop();
+    andrew.tinyMenuShowing = false;
+    andrew.navStay();
 });
